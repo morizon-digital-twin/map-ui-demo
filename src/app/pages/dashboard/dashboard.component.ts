a65686b35e72eea26ace627846b3c1a2c57bf810
Nico Rudolf <nico.rudolf1@gmx.de>
2022-11-15 14:53:29 +0100

    added dashboard, routing api call. major fixes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
+import { SelectorMatcher } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js';
+
+
 
 
 @Component({
@@ -12,11 +15,229 @@ export class DashboardComponent implements OnInit{
 
   public canvas : any;
   public ctx;
+  selectedDistrict1; 
   public chartColor;
   public chartEmail;
   public chartHours;
 
+  listOptions = [
+    { id: 0, name: "Geußnitz" },
+    { id: 1, name: "Kayna" },
+    { id: 2, name: "Luckenau" },
+    { id: 3, name: "Nonnewitz" },
+    { id: 4, name: "Würchwitz" },
+    { id: 5, name: "Zangenberg" },
+    { id: 6, name: "Zeitz" }
+  ];
+
+  zeitz = {
+    einwohner: 22564,
+    u25: {
+      m: 2178,
+      w: 1980,
+    },
+
+    b25_45: {
+      m: 1876,
+      w: 1971,
+
+    },
+    b45_65: {
+      m: 3918,
+      w: 3806,
+
+    },
+    u65: {
+      m: 2700,
+      w: 4135,
+    }
+  };
+
+
+  geussnitz = {
+    einwohner: 530,
+    u25: {
+      m: 46,
+      w: 41,
+    },
+
+    b25_45: {
+      m: 48,
+      w: 30,
+
+    },
+    b45_65: {
+      m:112,
+      w: 94,
+
+    },
+    u65: {
+      m: 60,
+      w: 99,
+    }
+  };
+
+
+
+  luckenau = {
+    einwohner: 438,
+    u25: {
+      m: 44,
+      w: 48,
+    },
+
+    b25_45: {
+      m: 43,
+      w: 31,
+
+    },
+    b45_65: {
+      m: 57,
+      w: 71,
+
+    },
+    u65: {
+      m: 60,
+      w: 84,
+    }
+  };
+
+  kayna = {
+    einwohner: 1155,
+    u25: {
+      m: 105,
+      w: 98,
+    },
+
+    b25_45: {
+      m: 96,
+      w: 99,
+
+    },
+    b45_65: {
+      m: 222,
+      w: 190,
+
+    },
+    u65: {
+      m: 139,
+      w: 206,
+    }
+  };
+
+  
+
+
+  nonnewitz = {
+    einwohner: 579,
+    u25: {
+      m: 62,
+      w: 44,
+    },
+
+    b25_45: {
+      m: 38,
+      w: 44,
+
+    },
+    b45_65: {
+      m: 108,
+      w: 103,
+
+    },
+    u65: {
+      m: 77,
+      w: 103,
+    }
+  };
+
+
+
+theissen = {
+    einwohner: 1694,
+    u25: {
+      m: 164,
+      w: 135,
+    },
+
+    b25_45: {
+      m: 156,
+      w: 137,
+
+    },
+    b45_65: {
+      m: 269,
+      w: 323,
+
+    },
+    u65: {
+      m: 199,
+      w: 311,
+    }
+  };
+
+
+  wuerchwitz = {
+    einwohner: 491,
+    u25: {
+      m: 53,
+      w: 29,
+    },
+
+    b25_45: {
+      m: 49,
+      w: 43,
+
+    },
+    b45_65: {
+      m: 82,
+      w: 84,
+
+    },
+    u65: {
+      m: 54,
+      w: 97,
+    }
+  };
+
+zangenberg = {
+    einwohner: 335,
+    u25: {
+      m: 31,
+      w: 29,
+    },
+
+    b25_45: {
+      m: 29,
+      w: 30,
+
+    },
+    b45_65: {
+      m: 53,
+      w: 52,
+
+    },
+    u65: {
+      m: 50,
+      w: 61,
+    }
+  }; 
+
+
+
+
     ngOnInit(){
+
+
+
+
+
+    
+
+
+
+
+
       this.chartColor = "#FFFFFF";
 
       this.canvas = document.getElementById("chartHours");
@@ -26,14 +247,15 @@ export class DashboardComponent implements OnInit{
         type: 'line',
 
         data: {
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+          labels: ["Geußnitz", "Kayna", "Luckenau", "Nonnewitz", "Theißen", "Würchwitz", "Zangenberg", "Zeitz"],
           datasets: [{
               borderColor: "#6bd098",
               backgroundColor: "#6bd098",
               pointRadius: 0,
               pointHoverRadius: 0,
               borderWidth: 3,
-              data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
+              data: [( this.geussnitz.u25.m + this.geussnitz.u25.w ) / this.geussnitz.einwohner, (this.kayna.u25.m + this.kayna.u25.w)/ this.kayna.einwohner, ( this.luckenau.u25.m + this.luckenau.u25.w ) /this.luckenau.einwohner, ( this.nonnewitz.u25.m + this.nonnewitz.u25.w ) / this.nonnewitz.einwohner , ( this.theissen.u25.m + this.theissen.u25.w ) / this.theissen.einwohner, 
+              ( this.wuerchwitz.u25.m + this.wuerchwitz.u25.w ) / this.wuerchwitz.einwohner, ( this.zangenberg.u25.m + this.zangenberg.u25.w ) / this.zangenberg.einwohner, ( this.zeitz.u25.m + this.zeitz.u25.w ) / this.zeitz.einwohner]
             },
             {
               borderColor: "#f17e5d",
@@ -41,7 +263,8 @@ export class DashboardComponent implements OnInit{
               pointRadius: 0,
               pointHoverRadius: 0,
               borderWidth: 3,
-              data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
+              data: [( this.geussnitz.b25_45.m + this.geussnitz.b25_45.w ) / this.geussnitz.einwohner, (this.kayna.b25_45.m + this.kayna.b25_45.w)/ this.kayna.einwohner, ( this.luckenau.b25_45.m + this.luckenau.b25_45.w ) /this.luckenau.einwohner, ( this.nonnewitz.b25_45.m + this.nonnewitz.b25_45.w ) / this.nonnewitz.einwohner , ( this.theissen.b25_45.m + this.theissen.b25_45.w ) / this.theissen.einwohner, 
+              ( this.wuerchwitz.b25_45.m + this.wuerchwitz.b25_45.w ) / this.wuerchwitz.einwohner, ( this.zangenberg.b25_45.m + this.zangenberg.b25_45.w ) / this.zangenberg.einwohner, ( this.zeitz.b25_45.m + this.zeitz.b25_45.w ) / this.zeitz.einwohner]
             },
             {
               borderColor: "#fcc468",
@@ -49,7 +272,17 @@ export class DashboardComponent implements OnInit{
               pointRadius: 0,
               pointHoverRadius: 0,
               borderWidth: 3,
-              data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
+              data: [( this.geussnitz.b45_65.m + this.geussnitz.b45_65.w ) / this.geussnitz.einwohner, (this.kayna.b45_65.m + this.kayna.b45_65.w)/ this.kayna.einwohner, ( this.luckenau.b45_65.m + this.luckenau.b45_65.w ) /this.luckenau.einwohner, ( this.nonnewitz.b45_65.m + this.nonnewitz.b45_65.w ) / this.nonnewitz.einwohner , ( this.theissen.b45_65.m + this.theissen.b45_65.w ) / this.theissen.einwohner, 
+              ( this.wuerchwitz.b45_65.m + this.wuerchwitz.b45_65.w ) / this.wuerchwitz.einwohner, ( this.zangenberg.b45_65.m + this.zangenberg.b45_65.w ) / this.zangenberg.einwohner, ( this.zeitz.b45_65.m + this.zeitz.b45_65.w ) / this.zeitz.einwohner]
+            },
+            {
+              borderColor: "#457ac0",
+              backgroundColor: "#457ac0",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              borderWidth: 3,
+              data: [( this.geussnitz.u65.m + this.geussnitz.u65.w ) / this.geussnitz.einwohner, (this.kayna.u65.m + this.kayna.u65.w)/ this.kayna.einwohner, ( this.luckenau.u65.m + this.luckenau.u65.w ) /this.luckenau.einwohner, ( this.nonnewitz.u65.m + this.nonnewitz.u65.w ) / this.nonnewitz.einwohner , ( this.theissen.u65.m + this.theissen.u65.w ) / this.theissen.einwohner, 
+              ( this.wuerchwitz.u65.m + this.wuerchwitz.u65.w ) / this.wuerchwitz.einwohner, ( this.zangenberg.u65.m + this.zangenberg.u65.w ) / this.zangenberg.einwohner, ( this.zeitz.u65.m + this.zeitz.u65.w ) / this.zeitz.einwohner]
             }
           ]
         },
@@ -95,16 +328,16 @@ export class DashboardComponent implements OnInit{
           },
         }
       });
-
+      this.selectedDistrict1 = this.zeitz;
 
       this.canvas = document.getElementById("chartEmail");
       this.ctx = this.canvas.getContext("2d");
       this.chartEmail = new Chart(this.ctx, {
         type: 'pie',
         data: {
-          labels: [1, 2, 3],
+          labels: ['U25', '25-45', '45-65', '65+'],
           datasets: [{
-            label: "Emails",
+            label: "Alterstruktur",
             pointRadius: 0,
             pointHoverRadius: 0,
             backgroundColor: [
@@ -114,15 +347,16 @@ export class DashboardComponent implements OnInit{
               '#ef8157'
             ],
             borderWidth: 0,
-            data: [342, 480, 530, 120]
+            data: [this.selectedDistrict1.u25.m+this.selectedDistrict1.u25.w, this.selectedDistrict1.b25_45.m+this.selectedDistrict1.b25_45.w, this.selectedDistrict1.b45_65.m+this.selectedDistrict1.b45_65.w, this.selectedDistrict1.u65.m+this.selectedDistrict1.u65.w,]
           }]
         },
 
         options: {
 
           legend: {
-            display: false
+            display: true
           },
+          
 
           pieceLabel: {
             render: 'percentage',
@@ -131,7 +365,147 @@ export class DashboardComponent implements OnInit{
           },
 
           tooltips: {
-            enabled: false
+            enabled: true
+          },
+
+          scales: {
+            yAxes: [{
+
+              ticks: {
+                display: false
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineColor: "transparent",
+                color: 'rgba(255,255,255,0.05)'
+              }
+
+            }],
+
+            xAxes: [{
+              barPercentage: 1.6,
+              gridLines: {
+                drawBorder: false,
+                color: 'rgba(255,255,255,0.1)',
+                zeroLineColor: "transparent"
+              },
+              ticks: {
+                display: false,
+              }
+            }]
+          },
+        }
+      }); 
+
+      var speedCanvas = document.getElementById("speedChart");
+
+      var dataFirst = {
+        data: [this.zeitz.u25.w, this.zeitz.b25_45.w, this.zeitz.b45_65.w, this.zeitz.u65.w],
+        fill: false,
+        borderColor: '#fbc658',
+        backgroundColor: 'transparent',
+        pointBorderColor: '#fbc658',
+        pointRadius: 4,
+        pointHoverRadius: 4,
+        pointBorderWidth: 8,
+      };
+
+      var dataSecond = {
+        data: [this.zeitz.u25.m, this.zeitz.b25_45.m, this.zeitz.b45_65.m, this.zeitz.u65.m],
+        fill: false,
+        borderColor: '#51CACF',
+        backgroundColor: 'transparent',
+        pointBorderColor: '#51CACF',
+        pointRadius: 4,
+        pointHoverRadius: 4,
+        pointBorderWidth: 8
+      };
+
+      var speedData = {
+        labels: ['U25', '25-45', '45-65', '65+'],
+        datasets: [dataFirst, dataSecond]
+      };
+
+      var chartOptions = {
+        legend: {
+          display: false,
+          position: 'top'
+        }
+      };
+
+      var lineChart = new Chart(speedCanvas, {
+        type: 'line',
+        hover: false,
+        data: speedData,
+        options: chartOptions
+      });
+    }
+
+
+    selectChange( $event) {
+    
+
+        switch($event) {
+          case 0:
+
+          this.selectedDistrict1 = this.geussnitz; 
+
+          case 1:
+            this.selectedDistrict1 = this.kayna; 
+          case 2:
+            this.selectedDistrict1 = this.luckenau; 
+          case 3:
+            this.selectedDistrict1 = this.nonnewitz; 
+            case 4:
+              this.selectedDistrict1 = this.wuerchwitz; 
+              case 5:
+                this.selectedDistrict1 = this.zangenberg;
+                case 6:
+                  this.selectedDistrict1 = this.zeitz;
+
+     
+
+        }
+
+      console.log(this.selectedDistrict1); 
+
+
+      this.canvas = document.getElementById("chartEmail");
+      this.ctx = this.canvas.getContext("2d");
+      this.chartEmail = new Chart(this.ctx, {
+        type: 'pie',
+        data: {
+          labels: ['U25', '25-45', '45-65', '65+'],
+          datasets: [{
+            label: "Alterstruktur",
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            backgroundColor: [
+              '#e3e3e3',
+              '#4acccd',
+              '#fcc468',
+              '#ef8157'
+            ],
+            borderWidth: 0,
+            data: [this.selectedDistrict1.u25.m+this.selectedDistrict1.u25.w, this.selectedDistrict1.b25_45.m+this.selectedDistrict1.b25_45.w, this.selectedDistrict1.b45_65.m+this.selectedDistrict1.b45_65.w, this.selectedDistrict1.u65.m+this.selectedDistrict1.u65.w,]
+          }]
+        },
+
+        options: {
+
+          legend: {
+            display: true
+          },
+          
+
+          pieceLabel: {
+            render: 'percentage',
+            fontColor: ['white'],
+            precision: 2
+          },
+
+          tooltips: {
+            enabled: true
           },
 
           scales: {
@@ -162,48 +536,6 @@ export class DashboardComponent implements OnInit{
           },
         }
       });
-
-      var speedCanvas = document.getElementById("speedChart");
-
-      var dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
-
-      var dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
-
-      var speedData = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [dataFirst, dataSecond]
-      };
-
-      var chartOptions = {
-        legend: {
-          display: false,
-          position: 'top'
-        }
-      };
-
-      var lineChart = new Chart(speedCanvas, {
-        type: 'line',
-        hover: false,
-        data: speedData,
-        options: chartOptions
-      });
+      
     }
 }
--- src/app/pages/typography/typography.component.ts
+++ src/app/pages/typography/typography.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 
-import * as ortsteile from "../../../assets/shapedata/Ortsteile_Zeitz.json";
-import * as einheitsgemeinden from "../../../asset/shapedata/Einheitsgemeinden.json";
-import * as zeitz from "../../../asset/shapedata/Zeitz_Bevoelkerung.json";
+import * as ortsteile from "../../../assets/shapedata/OrtsteileZeitz.json";
+
 
 
 
 import * as L from 'leaflet';
-import { each } from 'jquery';
+
+import * as zeitz from "../../../assets/shapedata/ZeitzBevoelkerung.json";
+import * as gemeinden from "../../../assets/shapedata/Einheitsgemeinden.json";
+import { data, each } from 'jquery';
 import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { waitForAsync } from '@angular/core/testing';
 
 
 
@@ -25,6 +28,8 @@ export class TypographyComponent implements AfterViewInit {
   private map;
   markers: L.Marker[] = [];
   layers: L.Layer[] = [];
+
+  duration = [];
   myMarkers = [];
   private geojsonLayer;
   hospitalBool = false;
@@ -37,23 +42,149 @@ export class TypographyComponent implements AfterViewInit {
   geojsonLayer2: L.GeoJSON<any>;
   geojsonLayer3: L.GeoJSON<any>;
   shapelayers: L.Layer[] = [];
+  hospitalMarker: L.Marker;
+  circle: L.Layer;
+  distance = [];
 
 
- 
-  gardenStyle( ) {
+
+  gardenStyle() {
     return {
       fillColor: "#FF00FF",
-      fillOpacity: 1,
+      fillOpacity: 0.1,
       color: '#B04173',
     };
   }
-  
- 
+
+  gardenStyle2() {
+    return {
+      fillColor: "green",
+      fillOpacity: 0.01,
+      color: 'green',
+    };
+  }
+
+  gardenStyle3() {
+    return {
+      fillColor: "blue",
+      fillOpacity: 0.1,
+      color: 'blue',
+    };
+  }
+
+
+  drawCircle() {
+
+
+
+    if (this.hospitalBool) {
+      var lat = this.markers[0].getLatLng().lat;
+      var lng = this.markers[0].getLatLng().lng;
+
+      this.circle = L.circle([lat, lng], 5000).addTo(this.map);
+
+    }
+
+  }
+
+
 
   private initMap(): void {
+
+
+
+    var jsonString = JSON.stringify(zeitz);
+    var jsonOBJ = JSON.parse(jsonString);
+    this.geojsonLayer = new L.GeoJSON(jsonOBJ);
+
+    var jsonString2 = JSON.stringify(gemeinden);
+    var jsonOBJ2 = JSON.parse(jsonString2);
+    this.geojsonLayer2 = new L.GeoJSON(jsonOBJ2);
+
+    var jsonString3 = JSON.stringify(ortsteile);
+    var jsonOBJ3 = JSON.parse(jsonString3);
+    this.geojsonLayer3 = new L.GeoJSON(jsonOBJ3);
+
+
+
+
+
+    // this.geojsonLayer.addTo(this.map);
+
+    //var shapes = L.layerGroup([this.geojsonLayer, this.geojsonLayer2, this.geojsonLayer3]);
+
+    var shapeLayer = L.geoJSON(jsonOBJ, {
+      style: this.gardenStyle,
+
+      /* onEachFeature: function (feature, layer) {
+         layer.bindPopup('<h1>'+feature.properties.f1+'</h1><p>name: '+feature.properties.f2+'</p>'); }*/
+
+
+      onEachFeature: function popUp(f, l) {
+        var out = [];
+        if (f.properties) {
+          for (var key in f.properties) {
+            out.push(key + ": " + f.properties[key]);
+          }
+          l.bindPopup(out.join("<br />"));
+        }
+      }
+
+    });
+
+    var shapeLayer2 = L.geoJSON(jsonOBJ2, {
+      style: this.gardenStyle2,
+
+      onEachFeature: function popUp(f, l) {
+        var out = [];
+        if (f.properties) {
+          for (var key in f.properties) {
+            out.push(key + ": " + f.properties[key]);
+          }
+          l.bindPopup(out.join("<br />"));
+        }
+      }
+
+
+
+
+
+    });
+
+    var shapeLayer3 = L.geoJSON(jsonOBJ3, {
+      style: this.gardenStyle3,
+
+      onEachFeature: function popUp(f, l) {
+        var out = [];
+        if (f.properties) {
+          for (var key in f.properties) {
+            out.push(key + ": " + f.properties[key]);
+          }
+          l.bindPopup(out.join("<br />"));
+        }
+      }
+    });
+
+
+    //var layerMap = L.layerGroup([shapeLayer, shapeLayer2 ]); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     this.map = L.map('map', {
       center: [51.04962, 12.1369], // "Zeitz"
       zoom: 10,
+      //layers: [ layerMap ]
 
     });
 
@@ -65,6 +196,19 @@ export class TypographyComponent implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
+
+
+
+    var overlayMaps = {
+      "none": L.tileLayer(''),
+      // "Zeitz": shapeLayer,
+      "Gemeinden": shapeLayer2,
+      "Ortsteile": shapeLayer3,
+    };
+
+    var layerControl = L.control.layers(overlayMaps).addTo(this.map);
+
+    //  shapeLayer.addTo(this.map); 
 
 
 
@@ -80,26 +224,26 @@ export class TypographyComponent implements AfterViewInit {
   }
 
   addLayer() {
-   /* var jsonString = JSON.stringify(shp3);
-    var jsonOBJ = JSON.parse(jsonString);
-
-    this.geojsonLayer = new L.GeoJSON(jsonOBJ);
-
-
-    this.geojsonLayer.addTo(this.map);
-
-
-    this.geojsonLayer.on('click', function (e) {
-      console.log(e.layer);
-      if (e.layer.selected) {
-
-        e.layer.selected = false;
-      } else {
-
-        e.layer.selected = true;
-      }
-    });
-*/
+    /* var jsonString = JSON.stringify(shp3);
+     var jsonOBJ = JSON.parse(jsonString);
+ 
+     this.geojsonLayer = new L.GeoJSON(jsonOBJ);
+ 
+ 
+     this.geojsonLayer.addTo(this.map);
+ 
+ 
+     this.geojsonLayer.on('click', function (e) {
+       console.log(e.layer);
+       if (e.layer.selected) {
+ 
+         e.layer.selected = false;
+       } else {
+ 
+         e.layer.selected = true;
+       }
+     });
+ */
 
 
 
@@ -117,34 +261,68 @@ export class TypographyComponent implements AfterViewInit {
 
   }
 
+  clearLayerOnDrag() {
+    for (var i = 0; i < this.layers.length; i++) {
+      console.log(this.layers[i]);
+      this.map.removeLayer(this.layers[i]);
+    }
+   
+    this.layers = [];
+
+    for (var i = 0; i < this.myMarkers.length; i++) {
+      this.myMarkers[i].distance = 0; 
+      this.myMarkers[i].duration = 0;
+    }
+
+  }
+
 
 
   clearMarkers() {
+  
+    this.markerCounter = 1;
+    this.hospitalBool = false;
+
+    
+
+
     for (var i = 0; i < this.markers.length; i++) {
       this.map.removeLayer(this.markers[i]);
     }
+
+
+ 
     for (var i = 0; i < this.layers.length; i++) {
       console.log(this.layers[i]);
       //this.layers[i].clearLayers();
       this.map.removeLayer(this.layers[i]);
     }
 
-    this.map.eachLayer(function (layer) {
+    
+
+    //this.map.removeLayer(this.circle);
+
+
+  
+
+   /* this.map.eachLayer(function (layer) {
       console.log(layer);
       if (layer instanceof L.Layer) {
         this.map.removeLayer(layer)
       }
-    })
-
+    })*/
+   
     this.layers = [];
     this.markers = [];
+    this.markers.length = 0; 
     this.myMarkers = [];
-    this.markerCounter = 1;
-    this.hospitalBool = false;
+
+    
 
   }
 
-  calculateRoute() {
+
+  async calculateRoute() {
     console.log(this.myMarkers);
     // hospital
 
@@ -153,55 +331,72 @@ export class TypographyComponent implements AfterViewInit {
     var lng = this.markers[0].getLatLng().lng;
 
     var urlBase = 'https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf62480ebc930105be49ff8b156131f6895d6f&start=' + lng + ',' + lat;
+    var urlBase2 = 'https://osrm.bunsencloud.de/route/v1/driving/' + lng + "," + lat; // "12.118548129879468,51.07980746234024;12.140293815251148,51.04451465548099?steps=true'
 
     for (var i = 1; i < this.markers.length; i++) {
 
       var url = urlBase + '&end=' + this.markers[i].getLatLng().lng + ',' + this.markers[i].getLatLng().lat;
+
+      const markers = this.myMarkers;
       this.http.get<any>(url).subscribe(data => {
-        console.log(data)
-       
-        var gardenLayer =  L.geoJSON(data     ,{
+
+
+        var gardenLayer = L.geoJSON(data, {
           style: this.gardenStyle
-         
-        });    
-                       
+
+        });
+
         gardenLayer.addTo(this.map);
         this.layers.push(gardenLayer);
 
+
+
+        this.duration.push(data.features[0].properties.summary.duration);
+        this.distance.push(data.features[0].properties.summary.distance);
+
+        console.log("duration zero");
+        console.log(this.duration.length);
+
+
+
+
+
+
+
+
+
       })
 
-
     }
+    this.delay(4000);
   }
 
+  async delay(ms: number) {
+    await new Promise<void>(resolve => setTimeout(() => resolve(), ms)).then(() => {
+
+      for (var i = 0; i < this.duration.length; i++) {
+        //ignore hospital
+        this.myMarkers[i + 1].duration = this.duration[i] ;
+        this.myMarkers[i + 1].distance = ( this.distance[i] / 1000 ) + ' km';
+        
+      }
+    });
+  }
 
 
   ngAfterViewInit(): void {
     this.initMap();
 
 
-    var jsonString = JSON.stringify(einheitsgemeinden);
-    var jsonOBJ = JSON.parse(jsonString); 
+    var jsonString = JSON.stringify(zeitz);
+    var jsonOBJ = JSON.parse(jsonString);
 
-    this.geojsonLayer = new L.GeoJSON(jsonOBJ);   
+    this.geojsonLayer = new L.GeoJSON(jsonOBJ);
 
-  
-   // this.geojsonLayer.addTo(this.map);
 
-    //var shapes = L.layerGroup([this.geojsonLayer, this.geojsonLayer2, this.geojsonLayer3]);
 
-    var shapeLayer =  L.geoJSON(jsonOBJ   ,{
-      //style: this.gardenStyle
-     
-    });    
-   
 
-    
-    
 
-  
-    shapeLayer.addTo(this.map); 
-    shapeLayer.on('click', function(e) { console.log(e) });
 
 
     this.map.on("click", e => {
@@ -210,12 +405,15 @@ export class TypographyComponent implements AfterViewInit {
 
         var myMarker = {
           type: "hospital",
-          title: "Krankenhaus",
+          title: "Ärztehaus",
           lat: e.latlng.lat,
-          lng: e.latlng.lng
+          lng: e.latlng.lng,
+          distance: 0,
+          duration: 0
+
         }
         this.myMarkers.push(myMarker);
-        var newMarker = L.marker([e.latlng.lat, e.latlng.lng],
+        this.hospitalMarker = L.marker([e.latlng.lat, e.latlng.lng],
           {
             draggable: true,
             autoPan: true, icon: L.icon({
@@ -227,21 +425,60 @@ export class TypographyComponent implements AfterViewInit {
               iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/layers.png",
               shadowUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png"
             })
-          },).bindTooltip("Krankenhaus",
+          },).bindTooltip("Ärztehaus",
             {
               permanent: true,
               direction: 'right'
             }).addTo(this.map); // add the marker onclick
         this.hospitalBool = true;
-        this.markers.push(newMarker);
+        //const clearLayerOnDrag = this.clearLayerOnDrag();
+        var layers = this.layers;
+        var myMarkers = this.myMarkers;
+        this.hospitalMarker.on('drag', function (e) {
+          console.log("hello world"); 
+          
+          console.log(layers); 
+          for (var i = 0; i < layers.length; i++) {
+            console.log(layers[i]);
+            this.map.removeLayer(this.layers[i]);
+          }
+         
+          layers = [];
+          console.log(layers); 
+      
+          for (var i = 0; i < myMarkers.length; i++) {
+           myMarkers[i].distance = 0; 
+            myMarkers[i].duration = 0;
+          }
+
+          console.log(myMarkers); 
+
+          
+         
+
+          
+
+
+
+
+
+
+      });
+      this.hospitalMarker.on('click', L.DomEvent.stopPropagation);
+
+        this.markers.push(this.hospitalMarker);
+
       } else {
+
 
         var title = "Ort " + this.markers.length
         var myMarker = {
           type: "location",
           title: title,
           lat: e.latlng.lat,
-          lng: e.latlng.lng
+          lng: e.latlng.lng,
+          distance: 0,
+          duration: 0
         }
         this.myMarkers.push(myMarker);
         var newMarker = L.marker([e.latlng.lat, e.latlng.lng],
@@ -264,33 +501,17 @@ export class TypographyComponent implements AfterViewInit {
 
         this.markers.push(newMarker);
 
+    
 
       }
+
+
+
     });
-
-
-
-
-
-    //shapes.addTo(this.map);
-
-
-    //this.geojsonLayer.addTo(this.map);
-
-
-    /*   this.map.on("click", e => {      
-         this.map.zoomIn();
-         this.map.panTo([e.latlng.lat, e.latlng.lng]);    
-       });*/
-
+ 
 
   }
 }
 
 
-
-
-function gardenOnEachFeature(feature: any, layer: any) {
-  throw new Error('Function not implemented.');
-}
 
